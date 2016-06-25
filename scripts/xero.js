51a232034aece760cebc5afd530d90ab9b82d62c
Russell Dear <you@example.com>
2016-06-25 18:02:42 +1200

    NewRelic

--- scripts/xero.js
+++ scripts/xero.js
@@ -1,18 +1,15 @@
 
 module.exports = function(robot) {
     
-    robot.respond(/xero( (\w+))?/i, function(msg) {
+    robot.respond(/xero get( (\w+))?/i, function(msg) {
+
+        require('newrelic');
 
         var RSA_PRIVATE_KEY = require('fs').readFileSync('privatekey.pem');
 
         var Xero = require('xero');
 
         var xero = new Xero('LXWNZMTOGBJKN9URNNUUBPLX5ARQAY', 'EVFUBNGJIYKRZCVN7LZZMZIYTBDLIT', RSA_PRIVATE_KEY);
-
-        console.log(msg.match[0]);
-        console.log(msg.match[1]);
-        console.log(msg.match[2]);
-        
 
         xero.call('GET', '/' + msg.match[2], null, function(err, json) {
             if (err) {
@@ -21,5 +18,27 @@ module.exports = function(robot) {
             }
             msg.reply(JSON.stringify(json));
         });
+    });
+
+    robot.respond(/xero create( (\w+))?/i, function(msg) {
+
+        require('newrelic');
+
+        var RSA_PRIVATE_KEY = require('fs').readFileSync('privatekey.pem');
+
+        var Xero = require('xero');
+
+        var xero = new Xero('LXWNZMTOGBJKN9URNNUUBPLX5ARQAY', 'EVFUBNGJIYKRZCVN7LZZMZIYTBDLIT', RSA_PRIVATE_KEY);
+
+        if (msg.match[2] == invoice){
+
+            xero.call('PUT', '/invoice', null, function(err, json) {
+                if (err) {
+                    log.error(err);
+                    msg.reply('Unable to contact Xero');
+                }
+                msg.reply(JSON.stringify(json));
+            });
+        }
     });
 };
