ed8bbdbf5477d3cd6b0bd4a9dab2ccd2fedefd15
Russell Dear <you@example.com>
2016-06-25 18:02:42 +1200

    NewRelic

--- scripts/xero.js
+++ scripts/xero.js
@@ -9,11 +9,6 @@ module.exports = function(robot) {
 
         var xero = new Xero('LXWNZMTOGBJKN9URNNUUBPLX5ARQAY', 'EVFUBNGJIYKRZCVN7LZZMZIYTBDLIT', RSA_PRIVATE_KEY);
 
-        console.log(msg.match[0]);
-        console.log(msg.match[1]);
-        console.log(msg.match[2]);
-        
-
         xero.call('GET', '/' + msg.match[2], null, function(err, json) {
             if (err) {
                 log.error(err);
