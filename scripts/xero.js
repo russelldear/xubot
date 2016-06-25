91645c16719c3364aafc3600c12e77494d1e2ac4
Russell Dear <you@example.com>
2016-06-25 15:51:44 +1200

    Hubotize

--- scripts/xero.js
+++ scripts/xero.js
@@ -1,16 +1,19 @@
 
-var RSA_PRIVATE_KEY = require('fs').readFileSync('privatekey.pem');
+module.exports = function(robot) {
+    
+    var RSA_PRIVATE_KEY = require('fs').readFileSync('privatekey.pem');
 
-var Xero = require('xero');
- 
-console.log('this is working');
+    var Xero = require('xero');
 
-var xero = new Xero('LXWNZMTOGBJKN9URNNUUBPLX5ARQAY', 'EVFUBNGJIYKRZCVN7LZZMZIYTBDLIT', RSA_PRIVATE_KEY);
+    console.log('this is working');
 
-xero.call('GET', '/Users', null, function(err, json) {
-        if (err) {
-            log.error(err);
-            return res.json(400, {error: 'Unable to contact Xero'});
-        }
-        return res.json(200, json);
-    });
+    var xero = new Xero('LXWNZMTOGBJKN9URNNUUBPLX5ARQAY', 'EVFUBNGJIYKRZCVN7LZZMZIYTBDLIT', RSA_PRIVATE_KEY);
+
+    xero.call('GET', '/Users', null, function(err, json) {
+            if (err) {
+                log.error(err);
+                return res.json(400, {error: 'Unable to contact Xero'});
+            }
+            return res.json(200, json);
+        });
+};
