c9fa429a2e2d6c261775eb61242ab8cbf4be62c1
Russell Dear <you@example.com>
2016-06-25 15:53:16 +1200

    Reply as msg

--- scripts/xero.js
+++ scripts/xero.js
@@ -10,10 +10,10 @@ module.exports = function(robot) {
     var xero = new Xero('LXWNZMTOGBJKN9URNNUUBPLX5ARQAY', 'EVFUBNGJIYKRZCVN7LZZMZIYTBDLIT', RSA_PRIVATE_KEY);
 
     xero.call('GET', '/Users', null, function(err, json) {
-            if (err) {
-                log.error(err);
-                return res.json(400, {error: 'Unable to contact Xero'});
-            }
-            return res.json(200, json);
-        });
+        if (err) {
+            log.error(err);
+            msg.reply('Unable to contact Xero');
+        }
+        msg.reply('Yuss');
+    });
 };
