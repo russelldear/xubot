58b46fc2adb506c07b09877bc08087bd519998f6
Russell Dear <you@example.com>
2016-07-01 19:46:00 +1200

    standup

--- scripts/standup.js
+++ scripts/standup.js
@@ -1,8 +1,7 @@
 module.exports = function(robot) {
     
     var CronJob = require('cron').CronJob;
-    new CronJob('00 45 19 * * 1-5', function() {
-        robot.messageRoom('xero/rdtest2', 'Standup time!');
-        console.log('You will see this message every second');
+    new CronJob('00 57 8 * * 1-5', function() {
+        robot.messageRoom('xero/api-engineering-internal', 'Standup time!');
     }, null, true, 'Pacific/Auckland');
 }
