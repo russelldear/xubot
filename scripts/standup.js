fa8b25c000de9b18c8e66778b5d4cedebef47c94
Russell Dear <you@example.com>
2016-07-01 19:20:40 +1200

    cron

--- scripts/standup.js
+++ scripts/standup.js
@@ -2,22 +2,14 @@ module.exports = function(robot) {
     
     robot.respond(/xero get( (\w+))?/i, function(msg) {
 
-robot.messageRoom("xero/rdtest2", "message");
-
-        require('newrelic');
-
-        var RSA_PRIVATE_KEY = require('fs').readFileSync('privatekey.pem');
-
-        var Xero = require('xero');
-
-        var xero = new Xero('LXWNZMTOGBJKN9URNNUUBPLX5ARQAY', 'EVFUBNGJIYKRZCVN7LZZMZIYTBDLIT', RSA_PRIVATE_KEY);
-
-        xero.call('GET', '/' + msg.match[2], null, function(err, json) {
-            if (err) {
-                log.error(err);
-                msg.reply('Unable to contact Xero');
-            }
-            msg.reply(JSON.stringify(json));
-        });
+        var cronJob = require('cron').CronJob;
+        var tz = 'Pacific/Auckland';
+	    new cronJob('0 0 19 21 * 1-5', workdaysNineAm, null, true, tz);
+  
+        var room = "xero/rdtest2";
+ 
+        var workdaysNineAm = function(){
+            robot.messageRoom(room, 'Standup time!');
+        }
     });
-};
+}
