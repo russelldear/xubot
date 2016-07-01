e91fda2300a6b74b9760a39c514495fae30c96d7
Russell Dear <you@example.com>
2016-07-01 19:58:52 +1200

    cards

---
+++ scripts/cards.js
@@ -0,0 +1,15 @@
+
+module.exports = function(robot) {
+    
+    robot.deck = '';
+
+    robot.respond(/cards new deck/i, function(msg) {
+
+        msg.http("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+            .get(function(err, res, body){
+                robot.deck = JSON.parse(body).deck_id;
+                msg.send('I\'ve shuffled a new deck of cards');
+                console.log(robot.deck);
+            });
+    });
+};
