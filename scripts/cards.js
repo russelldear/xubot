37310fb7817b802e4f89bb3da5795750f6a05121
Russell Dear <you@example.com>
2016-07-01 20:01:56 +1200

    debug

--- scripts/cards.js
+++ scripts/cards.js
@@ -7,6 +7,9 @@ module.exports = function(robot) {
 
         msg.http("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
             .get(function(err, res, body){
+                console.log(err);
+                console.log(res);
+                console.log(body);
                 robot.deck = JSON.parse(body).deck_id;
                 msg.send('I\'ve shuffled a new deck of cards');
                 console.log(robot.deck);
