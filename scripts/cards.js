1cbfa9f898f27c9b0ab6a58b067abdc5b37bb2aa
Russell Dear <you@example.com>
2016-07-02 09:13:39 +1200

    cards done

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,10 +1,46 @@
 module.exports = function(robot) {
-    
-    robot.respond(/cards new/i, function(msg) {
+  
+  var shuffled = 'New deck shuffled and ready.';
+  var deck_id = '';
 
-        robot.http("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-            .get(function(err, res, body){
-                console.log(body);
-            });
+  robot.respond(/cards new/i, function(msg) {
+    NewDeck();
+  });
+
+  robot.respond(/cards draw/i, function(msg) {
+
+    if (deck_id === ''){
+      NewDeck().then(function(result) {
+        DrawCard(msg);
+      },
+      function(err) {
+        console.log('Something didn\'t work: ' + err);
+      });
+    }
+    else {
+      DrawCard(msg);
+    }
+  });
+  
+  function NewDeck(){
+    return new Promise(function(resolve, reject) {
+      robot.http("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1").get()(function(err, res, body) {
+        deck_id = JSON.parse(body).deck_id;
+        if (err || deck_id === '') { 
+          reject(err); 
+        }
+        else {
+          console.log(shuffled);
+          resolve(); 
+        }
+      });
     });
+  }
+
+  function DrawCard(msg) {
+    robot.http("http://deckofcardsapi.com/api/deck/" + deck_id + "/draw/?count=1").get()(function(err, res, body) {
+        msg.send(JSON.parse(body).cards[0].image);
+    });
+  }
+
 };
