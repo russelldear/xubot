63f36ceea006d8f050fdbf2b2073ce002639ac8d
Russell Dear <you@example.com>
2016-07-01 19:38:19 +1200

    back to basics

--- scripts/standup.js
+++ scripts/standup.js
@@ -2,20 +2,9 @@ module.exports = function(robot) {
     
     robot.respond(/xero get( (\w+))?/i, function(msg) {
 
-        var cronJob = require('cron').CronJob;
-        var tz = 'Pacific/Auckland';
-	    new cronJob('0 0 19 23 * 1-5', workdaysNineAm, null, true, tz);
- 	    new cronJob('0 */1 * * * *', everyFiveMinutes, null, true, tz);
-  
-        var room = "xero/rdtest2";
- 
-        var workdaysNineAm = function(){
-            robot.messageRoom(room, 'Standup time!');
-        }
- 
-        var everyFiveMinutes = function(){
-            robot.messageRoom(room, 'Standup time!');
-        }
-        console.log('got here.');
+        var CronJob = require('cron').CronJob;
+            new CronJob('* * * * * *', function() {
+                console.log('You will see this message every second');
+            }, null, true, 'Pacific/Auckland');
     });
 }
