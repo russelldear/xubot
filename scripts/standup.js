7a9169fb923ab83b1ca3523161373945de637440
Russell Dear <you@example.com>
2016-07-01 19:10:12 +1200

    yeah?

--- scripts/standup.js
+++ scripts/standup.js
@@ -1,4 +1,23 @@
 module.exports = function(robot) {
     
-    robot.messageRoom("xero/rdtest2", "message");
+    robot.respond(/xero get( (\w+))?/i, function(msg) {
+
+robot.messageRoom("xero/rdtest2", "message");
+
+        require('newrelic');
+
+        var RSA_PRIVATE_KEY = require('fs').readFileSync('privatekey.pem');
+
+        var Xero = require('xero');
+
+        var xero = new Xero('LXWNZMTOGBJKN9URNNUUBPLX5ARQAY', 'EVFUBNGJIYKRZCVN7LZZMZIYTBDLIT', RSA_PRIVATE_KEY);
+
+        xero.call('GET', '/' + msg.match[2], null, function(err, json) {
+            if (err) {
+                log.error(err);
+                msg.reply('Unable to contact Xero');
+            }
+            msg.reply(JSON.stringify(json));
+        });
+    });
 };
