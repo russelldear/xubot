1110a034267405ebb1652d55b419b7c7bc8ffefe
Russell Dear <you@example.com>
2016-07-09 16:23:03 +1200

    Use old string literal method

--- scripts/help.js
+++ scripts/help.js
@@ -1,25 +1,25 @@
 module.exports = function(robot) {
 
-    var helpText = `
-@RemindMe {number} {unit of time}
-@RemindMe directions from {place} to {place}
-@RemindMe translate me from {language} to {language} {text}
-@RemindMe pug me
-@RemindMe {rock/paper/scissor/lizard/spock}
-@RemindMe ship it
-@RemindMe xero get {endpoint}
-
-Cards
-@RemindMe cards new
-@RemindMe cards draw
-
-Voting
-@RemindMe start vote {comma-separated options}
-@RemindMe vote {option text or number}
-@RemindMe show choices
-@RemindMe show votes
-@RemindMe end vote
-`
+    var helpText = '\
+@RemindMe {number} {unit of time}\
+@RemindMe directions from {place} to {place}\
+@RemindMe translate me from {language} to {language} {text}\
+@RemindMe pug me\
+@RemindMe {rock/paper/scissor/lizard/spock}\
+@RemindMe ship it\
+@RemindMe xero get {endpoint}\
+\
+Cards\
+@RemindMe cards new\
+@RemindMe cards draw\
+\
+Voting\
+@RemindMe start vote {comma-separated options}\
+@RemindMe vote {option text or number}\
+@RemindMe show choices\
+@RemindMe show votes\
+@RemindMe end vote\
+';
 
     robot.respond(/help/i, function(msg) {
         ShowHelp(msg);
