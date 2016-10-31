22c648d3664c9942c40419b6f55e7dd2cb09754b
Russell Dear <russell.dear@gmail.com>
2016-11-01 09:53:53 +1300

    Update scrum time

--- scripts/standup.js
+++ scripts/standup.js
@@ -1,7 +1,7 @@
 module.exports = function(robot) {
     
     var CronJob = require('cron').CronJob;
-    new CronJob('00 57 8 * * 1-5', function() {
-        robot.messageRoom('xero/api-engineering-internal', '@team : Standup time!');
+    new CronJob('00 42 9 * * 1-5', function() {
+        robot.messageRoom('xero/api-engineering-internal', '@team : Scrum-of-scrums time!');
     }, null, true, 'Pacific/Auckland'); 
 }
