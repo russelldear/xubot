6c4a29a408fdea179782e65a4f64e434f81fbf12
Russell Dear <russell.dear@gmail.com>
2016-11-09 10:05:09 +1300

    Update reminder time

--- scripts/standup.js
+++ scripts/standup.js
@@ -1,7 +1,7 @@
 module.exports = function(robot) {
     
     var CronJob = require('cron').CronJob;
-    new CronJob('00 42 9 * * 1-5', function() {
+    new CronJob('00 44 9 * * 1-5', function() {
         robot.messageRoom('xero/api-engineering-internal', '@team : Scrum-of-scrums time!');
     }, null, true, 'Pacific/Auckland'); 
 }
